4bcac55a00e892149aa7813b61f5a1b6fdc68fc0
Tod Thomson <todthomson@me.com>
2015-08-06 14:25:28 +1000

    basic store api (stores data in static hash set)

--- backend/PdTechDemo.MessageContracts/Store.cs
+++ backend/PdTechDemo.MessageContracts/Store.cs
@@ -1,8 +1,14 @@
-﻿namespace PdTechDemo.MessageContracts
+﻿using System;
+
+namespace PdTechDemo.MessageContracts
 {
     // e.g. Good Games Brisbane.
     public class Store
     {
+        public Guid Id { get; set; }
+
         public string Name { get; set; }
+
+        public string Location { get; set; }
     }
 }
--- backend/PdTechDemo.Web/Controllers/StoreController.cs
+++ backend/PdTechDemo.Web/Controllers/StoreController.cs
@@ -1,39 +1,67 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Web.Http;
+using PdTechDemo.MessageContracts;
 
 namespace PdTechDemo.Web.Controllers
 {
     public class StoreController : ApiController
     {
-        // GET: api/Store
-        public IEnumerable<string> Get()
+        // Temp data structure...
+        private static readonly HashSet<Store> Stores = new HashSet<Store>
         {
-            return new string[] { "value1", "value2" };
+            new Store
+            {
+                Id = new Guid("a9f93717-07b1-41c4-a8a2-f223b650f22e"),
+                Name = "Good Games",
+                Location = "Spring Hill"
+            },
+            new Store
+            {
+                Id = new Guid("d73e5952-e5a7-4465-9dca-d9a091aef9ac"),
+                Name = "Ace Comics & Games",
+                Location = "Annerley"
+            }
+        };
+
+        // GET: api/Store (List)
+        public Store[] Get()
+        {
+            return Stores.ToArray();
         }
 
-        // GET: api/Store/5
-        public string Get(int id)
+        // GET: api/Store/5 (Read)
+        public Store Get(Guid id)
         {
-            return "value";
+            return Stores.SingleOrDefault(s => s.Id == id);
         }
 
-        // POST: api/Store
-        public void Post([FromBody]string value)
+        // POST: api/Store (Create)
+        public void Post([FromBody] Store store)
         {
+            Stores.Add(store); // Assuming Guid not rubbish here for now...
         }
 
-        // PUT: api/Store/5
-        public void Put(int id, [FromBody]string value)
+        // PUT: api/Store/5 (Update)
+        public void Put([FromBody] Store store)
         {
+            var existingStore = Stores.SingleOrDefault(s => s.Id == store.Id);
+            if (existingStore != null) existingStore = store;
         }
 
-        // DELETE: api/Store/5
-        public void Delete(int id)
+        // PATCH: api/Store/5 (Partial Update)
+        public void Patch([FromBody] Store store)
         {
+            // As per Put for now...
+            var existingStore = Stores.SingleOrDefault(s => s.Id == store.Id);
+            if (existingStore != null) existingStore = store;
+        }
+
+        // DELETE: api/Store/5 (Destroy)
+        public void Delete(Guid id)
+        {
+            Stores.RemoveWhere(s => s.Id == id);
         }
     }
 }
